b625a0000c517dff91b227f353858b17d9ca510c
Joh Yoshida <johyoshida@gmail.com>
2022-02-01 18:58:15 -0800

    Disable medium and hard difficulties, and make easy start button more clear

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,6 +2,7 @@ import React, {
   Component
 } from 'react';
 import {
+  IoPlayOutline,
   IoHappyOutline,
   IoSadOutline,
   IoSkullOutline,
@@ -17,13 +18,14 @@ import RedoButton from "./RedoButton";
 class Controls extends Component {
 
   render() {
+    // Disabled medium and hard difficulties until I can find a better implementation
+    // <div className={"Cell"} title="Difficult" onClick={this.props.startGame.bind(this,"medium")} ><IoSadOutline size={25} /></div>
+    // <div className={"Cell"} title="Deadly" onClick={this.props.startGame.bind(this,"hard")} ><IoSkullOutline size={25} /></div>
     return (
       <div className="Controls row">
-        <div className={"Cell"} title="Easy" onClick={this.props.startGame.bind(this,"easy")} ><IoHappyOutline size={25} /></div>
-        <div className={"Cell"} title="Difficult" onClick={this.props.startGame.bind(this,"medium")} ><IoSadOutline size={25} /></div>
-        <div className={"Cell"} title="Deadly" onClick={this.props.startGame.bind(this,"hard")} ><IoSkullOutline size={25} /></div>
-        <div className={"Cell"} title="Solve puzzle" onClick={this.props.solve} ><IoColorWandOutline size={25} /></div>
-        <div className={"Cell"} title="Restart puzzle" onClick={this.props.restartGame} ><IoRefresh size={25} /></div>
+        <div className={"Cell"} title="Play" onClick={this.props.startGame.bind(this,"easy")} ><IoPlayOutline size={25} /></div>
+        <div className={"Cell"} title="Restart" onClick={this.props.restartGame} ><IoRefresh size={25} /></div>
+        <div className={"Cell"} title="Solve" onClick={this.props.solve} ><IoColorWandOutline size={25} /></div>
         <div className={"Cell"} title="Check against solution" onClick={this.props.compare} ><IoCheckmark size={25} /></div>
         <div title={this.props.mode === "pen" ? "Switch to pencil" : "Switch to pen"}>
           <ModeSelector
